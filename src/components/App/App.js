5e715eff04203be0620155cfc6e4c41ee04329ba
Casey Dallavalle <cbdallavalle@gmail.com>
2018-12-09 19:37:50 -0700

    Add parallax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <div className="big">
+        <div className="parallax-container">
         </div>
       </div>
     );
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,11 +4,14 @@ import './Contact.scss';
 export const Contact = () => {
   return (
     <div className='Contact'>
-      <a href="mailto:cbdallavalle@gmail.com" id='contact'>
+      <a href='mailto:cdallava3@gmail.com' >
+        <i className='fas fa-envelope-square icon' id='email'></i>
       </a>
-      <a href="https://www.linkedin.com/in/casey-dallavalle/" id='linkedIn'>
+      <a href='https://www.linkedin.com/in/casey-dallavalle/'>
+        <i className='fab fa-linkedin icon' id='linkedin'></i>
       </a>
-      <a href="https://github.com/cbdallavalle/" id='gitHub'>
+      <a href='https://github.com/cbdallavalle/'>
+        <i className='fab fa-github-square icon' id='github'></i>
       </a>
     </div>
   )
